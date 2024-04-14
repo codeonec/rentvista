892eb6fce444233207f3a9774eed8af2e15bcd1a
Ritul Patel <ritul.patel904@gmail.com>
2024-04-14 18:40:40 -0400

    Added validation for minimum one month of booking

--- Frontend/src/components/bookingModal.jsx
+++ Frontend/src/components/bookingModal.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Alert, Button, Modal } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useLogin } from "../contexts/login-context";
-import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from "@stripe/stripe-js";
 
 const BookingModal = ({ showModal, onCloseModal, listingId }) => {
     const { currentUser } = useLogin();
@@ -12,6 +12,7 @@ const BookingModal = ({ showModal, onCloseModal, listingId }) => {
     const [endDate, setEndDate] = useState(null);
     const [availableDates, setAvailableDates] = useState([]);
     const authToken = JSON.parse(localStorage.getItem("token"));
+    const [error, setError] = useState("");
 
     const fetchAvailableDates = async () => {
         try {
@@ -33,34 +34,44 @@ const BookingModal = ({ showModal, onCloseModal, listingId }) => {
     };
 
     const makePayment = async () => {
-        const stripe = await loadStripe("pk_test_51P3lmWBbv8rvOm4IigzxbKHxPRqmv5WFkAxios0x8U7pTHAQpo1ImrHoDjHwePRMVrgkAemlK9VdCeFyApPK4Rn700TY2Bc4Yf");
+        const stripe = await loadStripe(
+            "pk_test_51P3lmWBbv8rvOm4IigzxbKHxPRqmv5WFkAxios0x8U7pTHAQpo1ImrHoDjHwePRMVrgkAemlK9VdCeFyApPK4Rn700TY2Bc4Yf"
+        );
 
-        const response = await fetch("http://localhost:5000/booking/create-checkout-session", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ listingId })
-        });
+        const response = await fetch(
+            "http://localhost:5000/booking/create-checkout-session",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ listingId }),
+            }
+        );
 
         const session = await response.json();
 
-        const result = stripe.redirectToCheckout(
-            { sessionId: session.id }
-        );
+        const result = stripe.redirectToCheckout({ sessionId: session.id });
 
         if (result.error) {
             console.log(result.error.message);
         }
-    }
+    };
 
     const handleSubmit = async () => {
         if (!startDate || !endDate) {
             // If either start date or end date is not selected, do not proceed
             console.log("Please select both check-in and check-out dates.");
+            setError("Please select both check-in and check-out dates.");
             return;
         }
-
+        const durationInDays = Math.ceil(
+            (endDate - startDate) / (1000 * 60 * 60 * 24)
+        );
+        if (durationInDays < 30) {
+            setError("Minimum booking duration is one month");
+            return;
+        }
         try {
             makePayment();
 
@@ -96,8 +107,7 @@ const BookingModal = ({ showModal, onCloseModal, listingId }) => {
 
     <Button variant="primary" onClick={handleSubmit}>
         Checkout
-    </Button>
-
+    </Button>;
 
     useEffect(() => {
         fetchAvailableDates();
@@ -109,6 +119,7 @@ const BookingModal = ({ showModal, onCloseModal, listingId }) => {
                 <Modal.Title>Select Dates</Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                {error && <Alert variant="danger">{error}.</Alert>}
                 <div className="mb-3">
                     <label>Check-in Date:</label>&nbsp;
                     <DatePicker
@@ -139,7 +150,7 @@ const BookingModal = ({ showModal, onCloseModal, listingId }) => {
                 <Button variant="secondary" onClick={onCloseModal}>
                     Close
                 </Button>
-                <Button variant="primary" onClick={handleSubmit} >
+                <Button variant="primary" onClick={handleSubmit}>
                     Checkout
                 </Button>
             </Modal.Footer>
